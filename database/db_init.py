3f0efbf35a12b7f9f9b75458adf8800f50119e52
Rolvo <suphi.pembe@gmail.com>
2023-07-29 17:19:16 +0200

    db_init erster Table eingerichtet

---
+++ database/db_init.py
@@ -0,0 +1,20 @@
+import sqlite3
+
+#def create_database():
+con = sqlite3.connect("ALC.db")
+cur = con.cursor()
+
+Gebaeude = """
+    CREATE TABLE Gebaeude(
+        ObjektID PRIMARY KEY,
+        Postleitzahl INT(5),
+        Ortschaft VARCHAR(255),
+        Strasse VARCHAR(255),
+        Hausnummer INT(4),
+        Anzahl Wohnobjekte INT(3)
+    )
+"""
+cur.execute(Gebaeude)
+con.commit
+con.close
+print("create_database ausgeführt") #debug
--- main.py
+++ main.py
@@ -3,10 +3,10 @@ import tkinter as tk    #Zeichnet Fenster
 root = tk.Tk()  #erstellt Objekt des Fensters, genannt root
 
 root.title("Auto Landlord CRM") #Titel des Fensters
-root.minsize(width=800, height=400) #Mindestgröße des Fensters in px
+root.minsize(width=800, height=400) #Mindestgoeße des Fensters in px
 #root.maxsize()
-root.geometry("1280x720") #Standardgröße des Fensters in px
-root.resizable(width=True, height=True) #Einstellung ob die Größe des Fensters bearbeitbar sein soll
+root.geometry("1280x720") #Standardgroeße des Fensters in px
+root.resizable(width=True, height=True) #Einstellung ob die Groeße des Fensters bearbeitbar sein soll
 
 label1 = tk.Label(root, text = "Auto Landlord CRM") #erstelle Label in Parent root
 label1.pack()   #platziert label1 in root
