0a4769ca38f60abf9a2fd4428d4d2535fe942c72
Rolvo <suphi.pembe@gmail.com>
2023-07-30 01:32:29 +0200

    db_init Wohnobjekt & Mieter erstellt

--- database/db_init.py
+++ database/db_init.py
@@ -15,7 +15,41 @@ Gebaeude = """
         Anzahl Wohnobjekte INT(3)
     )
 """
+
+Wohnobjekt = """
+    CREATE TABLE Wohnobjekt(
+        ObjektID INT,
+        WohnobjektID PRIMARY KEY,
+        Groesse_in_m2 INT,
+        letzte_renovierung DATE,
+        anzahl_parkplatz INT,
+        FOREIGN KEY (ObjektID) REFERENCES Gebaeude(ObjektID),
+        FOREIGN KEY (ObjektID) REFERENCES Gebaeude(ObjektID)
+    )
+"""
+
+Mieter = """
+    CREATE TABLE Mieter(
+        ObjektID INT,
+        WohnobjektID INT,
+        MieterID PRIMARY KEY,
+        Vorname VARCHAR(255),
+        Nachname VARCHAR(255),
+        Geburtsdatum DATE,
+        old_Postleitzahl INT(4),
+        old_Strasse VARCHAR(255),
+        old_Hausnr INT(5),
+        old_Ortschaft VARCHAR(255),
+        Kaltmiete FLOAT,
+        Nebenkosten FLOAT,
+        Muellkosten FLOAT,
+        letzte_Mieterhöhung DATE
+    )
+"""
+
 cur.execute(Gebaeude)
+cur.execute(Wohnobjekt)
+cur.execute(Mieter)
 con.commit
 con.close
 print("create_database ausgeführt") #debug
