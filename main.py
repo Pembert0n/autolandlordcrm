ff9f4e451df87ee58cd29746660cdb0550bd02c7
Rolvo <suphi.pembe@gmail.com>
2023-07-31 13:01:37 +0200

    Gebäude entfernen Button + von .pack auf .grid in tkinter gewechselt

--- main.py
+++ main.py
@@ -25,6 +25,9 @@ root.minsize(width=800, height=400)  # Mindestgoeße des Fensters in px
 root.geometry("1280x720")  # Standardgroeße des Fensters in px
 root.resizable(width=True, height=True)  # Ob Groeße des Fensters bearbeitbar
 
+gebaeude_frame = tk.Frame(root)
+# erstelle Container um Buttons darin nebeneinander zu plazieren
+
 add_house_icon = Image.open("graphics/house-add-512.png").resize((50, 50))
 add_house_icon_tk = ImageTk.PhotoImage(add_house_icon)
 
@@ -36,8 +39,21 @@ gebaeude_add_button = tk.Button(root,
                                 background="gray74",
                                 command=empty_button)
 
-gebaeude_add_button.pack(anchor="w", pady=20, padx=20)
+gebaeude_add_button.grid(row=0, column=0, padx=20, pady=20)
 # anchor -> Ausrichtung nach Himmelsrichtung
 # pady/padx -> Seitenabstand
+
+remove_house_icon = Image.open("graphics/house-remove-512.png").resize((50, 50))
+remove_house_icon_tk = ImageTk.PhotoImage(remove_house_icon)
+
+gebaeude_remove_button = tk.Button(root,
+                                   text=" Gebäude entfernen ",
+                                   image=remove_house_icon_tk,
+                                   compound="right",  # verbindet Text & Bild
+                                   font=("Arial", 25),
+                                   background="gray74",
+                                   command=empty_button)
+
+gebaeude_remove_button.grid(row=0, column=1, padx=20, pady=20)
 
 root.mainloop()  # Ereignissschleife innerhalb des Fensters
