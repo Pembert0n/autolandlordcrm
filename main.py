73663a436283d1a18bb0b8d2e562ce7d80a33eb4
Rolvo <suphi.pembe@gmail.com>
2023-07-31 17:20:47 +0200

    initial commit branch single page application +code

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ import os
 from PIL import Image, ImageTk
 
 from database.db_init import create_database
-from logic import empty_button
+from pages import main_page
 
 if os.path.exists("ALC.db"):
     pass                # geh weiter in der Datei
@@ -25,24 +25,6 @@ root.minsize(width=800, height=400)  # Mindestgoeße des Fensters in px
 root.geometry("1280x720")  # Standardgroeße des Fensters in px
 root.resizable(width=True, height=True)  # Ob Groeße des Fensters bearbeitbar
 
-banner = tk.Label(root,
-                  text="ALC - Automated Landlord CRM",
-                  font=("Arial", 25),
-                  anchor="w")
-
-banner.grid(row=0, column=0, columnspan=1, padx=0, pady=0)
-
-check_house_icon = Image.open("graphics/house-30-512.png").resize((50, 50))
-check_house_icon_tk = ImageTk.PhotoImage(check_house_icon)
-
-gebaeude_add_button = tk.Button(root,
-                                text=" Gebäude Verwalten ",
-                                image=check_house_icon_tk,
-                                compound="right",  # verbindet Text & Bild
-                                font=("Arial", 25),
-                                background="gray74",
-                                command=empty_button)
-
-gebaeude_add_button.grid(row=1, column=0, columnspan=1, padx=20, pady=20)
+main_page(root)
 
 root.mainloop()  # Ereignissschleife innerhalb des Fensters
