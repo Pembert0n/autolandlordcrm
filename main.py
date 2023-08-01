9f8b93d4f81872a54a7ea779a1f4fe5a1f2659b9
Rolvo <suphi.pembe@gmail.com>
2023-08-01 10:07:57 +0200

    versuch Funktion zu in pages.py umzuverlegen

--- main.py
+++ main.py
@@ -10,22 +10,29 @@ import os
 from tkhtmlview import HTMLLabel
 # from PIL import Image, ImageTk
 from database.db_init import create_database
+# from pages import on_next_page_click, on_back_to_main_page_click
+
 
 def on_next_page_click():
-    # Ausführen des HTML Codes
+    """Ausführen des HTML Codes"""
     with open("html_pages/test_page.html", "r", encoding="utf-8") as file:
         html_test = file.read()
     startseite.set_html(html_test)
     next_test_page.grid_forget()  # Die Schaltfläche next_test_page ausblenden
-    back_to_main_page.grid(row=2, column=0, padx=20, pady=20)  # Die Schaltfläche back_to_main_page einblenden
+    back_to_main_page.grid(row=2, column=0, padx=20, pady=20)
+    # Die Schaltfläche back_to_main_page einblenden
+
 
 def on_back_to_main_page_click():
-    # Ausführen des HTML Codes für die Startseite
+    """Ausführen des HTML Codes für die Startseite"""
     with open("html_pages/main_page.html", "r", encoding="utf-8") as file:
         html_startseite = file.read()
     startseite.set_html(html_startseite)
-    back_to_main_page.grid_forget()  # Die Schaltfläche back_to_main_page ausblenden
-    next_test_page.grid(row=2, column=0, padx=20, pady=20)  # Die Schaltfläche next_test_page einblenden
+    back_to_main_page.grid_forget()
+    # Die Schaltfläche back_to_main_page ausblenden
+    next_test_page.grid(row=2, column=0, padx=20, pady=20)
+    # Die Schaltfläche next_test_page einblenden
+
 
 if os.path.exists("ALC.db"):
     # pass
@@ -52,6 +59,7 @@ startseite.grid(row=1, column=0, columnspan=1, padx=20, pady=20)
 next_test_page = tk.Button(root, text="pls go 2", command=on_next_page_click)
 next_test_page.grid(row=2, column=0, padx=20, pady=20)
 
-back_to_main_page = tk.Button(root, text="Back to Main Page", command=on_back_to_main_page_click)
+back_to_main_page = tk.Button(root, text="Back to Main Page",
+                              command=on_back_to_main_page_click)
 
 root.mainloop()
--- pages.py
+++ pages.py
@@ -1,9 +1,26 @@
-import tkinter as tk
-from tkhtmlview import HTMLLabel
-from main import root
+"""Probleme beim importieren der Variabeln,
+da sich diese in einem Loop befinden?"""
+# import tkinter as tk
+# from tkhtmlview import HTMLLabel
+from main import startseite, next_test_page, back_to_main_page
 
-def html_test_page():
-    # Ausführen des HTML Codes
+
+def on_next_page_click():
+    """Ausführen des HTML Codes"""
     with open("html_pages/test_page.html", "r", encoding="utf-8") as file:
         html_test = file.read()
-    return HTMLLabel(root, html=html_test, padx=20, pady=20)
+    startseite.set_html(html_test)
+    next_test_page.grid_forget()  # Die Schaltfläche next_test_page ausblenden
+    back_to_main_page.grid(row=2, column=0, padx=20, pady=20)
+    # Die Schaltfläche back_to_main_page einblenden
+
+
+def on_back_to_main_page_click():
+    """Ausführen des HTML Codes für die Startseite"""
+    with open("html_pages/main_page.html", "r", encoding="utf-8") as file:
+        html_startseite = file.read()
+    startseite.set_html(html_startseite)
+    back_to_main_page.grid_forget()
+    # Die Schaltfläche back_to_main_page ausblenden
+    next_test_page.grid(row=2, column=0, padx=20, pady=20)
+    # Die Schaltfläche next_test_page einblenden
